ba4118544c0c810e4f3545f4cd89b1678d4d6693
Joshua915420 <102965856+Joshua915420@users.noreply.github.com>
2022-08-03 23:26:50 +0100

    Add files via upload

--- software.php
+++ software.php
@@ -30,6 +30,18 @@
   <link href="css/responsive.css" rel="stylesheet">
 
 
+  <!-- Google tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-236526071-2">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-236526071-2');
+</script>
+
+
 
  
 </head>
